cc93cd51c5b4e0682e2e9dc1de1d75b7f99179d4
Shantanu Vishwanadha <thecoderpanda@shantanus-air.lan>
2022-12-16 14:55:46 +0530

    readme updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, Container, Row, Col} from 'reactstrap';
-import { QRCode } from "react-qr-svg";
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
+import { QRCodeSVG } from 'qrcode.react';
+
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import './App.css';
@@ -22,13 +23,13 @@ class App extends React.Component {
   handleChange(event) {
     event.preventDefault();
 
-    
+
   }
 
   handleSubmit = (event) => {
-    const base_string =`${this.state.base_string}`;
-    const sourceaddition = `?utm_source=${this.state.group_name}+${this.state.date_of_meetup}`; 
-    const medium= '&utm_medium=meetup';
+    const base_string = `${this.state.base_string}`;
+    const sourceaddition = `?utm_source=${this.state.group_name}+${this.state.date_of_meetup}`;
+    const medium = '&utm_medium=meetup';
     const dataString = base_string + sourceaddition + medium;
 
     this.setState({
@@ -46,14 +47,14 @@ class App extends React.Component {
       group_name: event.target.value
     })
   }
-  handledateChange= (event) => {
+  handledateChange = (event) => {
     this.setState({
       date_of_meetup: event.target.value
     })
   }
 
   printDocument() {
-    
+
     const input = document.getElementById('qr-code');
     console.log(input);
 
@@ -66,59 +67,63 @@ class App extends React.Component {
         // pdf.output('dataurlnewwindow');
         pdf.save("download.pdf");
       })
-    ;
+      ;
   }
 
   render() {
     return (
       <div className="App">
+
         <Container>
-          <Row style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+        <div className='logo'>
+        <img src='https://cdn.sanity.io/images/51n36hrp/facade/abb8930f5dd1892cc5b6311b697b3a81641a46f1-3637x875.png' alt='Covalent Logo' height={70} width={150}  />
+        </div>
+          <Row style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <Col>
-              <br/>
+              <br />
               <p>
                 Hi There To Generate QR Code fill in the below form.
               </p>
               <h3>Meetup Information</h3>
-                <Form onSubmit={this.handleSubmit}>
-                  <FormGroup>
-                    <Label for="name">Alchemist Name</Label>
-                    <Input type="text" placeholder="Enter Name" />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="class">Group Name</Label>
-                    <Input type="text" id="class" placeholder="Enter Community Name" ref="class" value={this.state.group_name} onChange={this.handlegroupChange} />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="section">Date of Meetup</Label>
-                    <Input type="date" id="section" placeholder="Enter Meetup Date " ref="section" value={this.state.section} onChange={this.handledateChange} />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="school">Venue</Label>
-                    <Input type="text" id="school" placeholder="Enter Venue Name" />
-                  </FormGroup>
-                  <Button type="submit">
-                    Generate QR Code
-                  </Button>
-                </Form>
-              </Col>
-              <Col>
-                <div id="print-this" >
-                      <br/>
-                      <div id="qr-code">
-                      <QRCode
-                          bgColor="#FFFFFF"
-                          fgColor="#000000"
-                          level="L"
-                          style={{ width: 200, height : 200 }}
-                          value={this.state.data}
-                      />
-                      </div>
-                      <br/>
-                      <Button onClick={this.printDocument}>You can Save Now</Button>
+              <Form onSubmit={this.handleSubmit}>
+                <FormGroup>
+                  <Label for="name">Alchemist Name</Label>
+                  <Input type="text" placeholder="Enter Name" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="class">Group Name</Label>
+                  <Input type="text" id="class" placeholder="Enter Community Name" ref="class" value={this.state.group_name} onChange={this.handlegroupChange} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="section">Date of Meetup</Label>
+                  <Input type="date" id="section" placeholder="Enter Meetup Date " ref="section" value={this.state.section} onChange={this.handledateChange} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="school">Venue</Label>
+                  <Input type="text" id="school" placeholder="Enter Venue Name" />
+                </FormGroup>
+                <Button type="submit">
+                  Generate QR Code
+                </Button>
+              </Form>
+            </Col>
+            <Col>
+              <div id="print-this" >
+                <br />
+                <div id="qr-code">
+                  <QRCodeSVG
+                    bgColor="#FFFFFF"
+                    fgColor="#000000"
+                    level="L"
+                    style={{ width: 200, height: 200 }}
+                    value={this.state.data}
+                  />
                 </div>
-                </Col>
-            </Row>
+                <br />
+                <Button onClick={this.printDocument}>You can Save Now</Button>
+              </div>
+            </Col>
+          </Row>
         </Container>
       </div>
     );
